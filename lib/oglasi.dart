2e7f82e328c0720ea165e4f0a2d2c29b6c0d63e0
Aleksa Zdravković <aleksazdravkovic@proton.me>
2023-12-09 20:15:01 +0100

    Rewarded oglas

--- lib/oglasi.dart
+++ lib/oglasi.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 class Oglasi {
-
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
@@ -12,21 +11,31 @@ class Oglasi {
     }
   }
 
-  static String get interstitialAdUnitId {
+  // static String get interstitialAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/1033173712";
+  //   // } else if (Platform.isIOS) {
+  //   //   return "ca-app-pub-3940256099942544/4411468910";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+
+  static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712";
-    // } else if (Platform.isIOS) {
-    //   return "ca-app-pub-3940256099942544/4411468910";
+      return "ca-app-pub-3940256099942544/5224354917";
+      // } else if (Platform.isIOS) {
+      //   return "ca-app-pub-3940256099942544/1712485313";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
 
-  static String get rewardedAdUnitId {
+  static String get nativeAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/5224354917";
-    // } else if (Platform.isIOS) {
-    //   return "ca-app-pub-3940256099942544/1712485313";
+      return "ca-app-pub-3940256099942544/2247696110";
+      // } else if (Platform.isIOS) {
+      //   return "ca-app-pub-3940256099942544/1712485313";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
